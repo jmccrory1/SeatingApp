1b06811a403fae96b7a5c79620212686d054785e
Julia McCrory <Julia.McCrory@ibm.com>
2020-07-23 16:36:19 -0500

    commit

--- queueapp/src/index.js
+++ queueapp/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import "./index.css";
+import express from "express";
+import React, { useState, useEffect } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = require("./database");
+const cors = require("cors");
+const app = express();
+
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
+
+const candidate = JSON.stringify(
+  require("./Artifacts/sample_candidate_info.json")
+);
+const breakout_room = JSON.stringify(
+  require("./Artifacts/sample_collection_breakout_room_info.json")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+app.use(cors());
+app.use(express.json());
+
+function App() {
+  const [client, setClient] = useState([]);
+
+  function RetrieveData() {
+    axios.get("http://localhost:5000/assign").then((response) => {
+      setClient(response.data.articles);
+    });
+  }
+  useEffect(() => {
+    RetrieveData();
+  }, []);
+
+
+  console.log(breakout_room);
+
+
+  return (
+    <div>
+      {client.map((item, index) => {
+        return (
+          <div id="parent">
+            <div
+              className="client info"key={index}>{candidate.client}
+              
+            </div>
+            
+
+           
+          </div>
+        )
+      }
+      )};
+    </div>
+  );
+}
+
+export default App;
